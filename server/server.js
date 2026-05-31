20556a97940a580760901e2108797f15a77dfb72
Seth Zarkovich <sethzarkovich@gmail.com>
2026-05-31 15:53:22 -0400

    adds basic error handler and health route

--- server/server.js
+++ server/server.js
@@ -12,8 +12,15 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
+app.get('/api/health', (req, res) => res.status(200).json({ status: 'ok' }));
+
 app.use('/api/gigs', gigsRouter);
 app.use('/api/venues', venuesRouter);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Something went wrong on the server." });
+});
 
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => console.log(`Server Running on PORT: ${PORT}`))
