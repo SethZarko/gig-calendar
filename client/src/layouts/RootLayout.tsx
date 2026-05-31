043ddc2b165d2d7e2cedd6c70e77aeec1c72c5db
Seth Zarkovich <sethzarkovich@gmail.com>
2026-05-31 18:44:52 -0400

    adds RootLayout

--- client/src/layouts/RootLayout.tsx
+++ client/src/layouts/RootLayout.tsx
@@ -1,6 +1,39 @@
+import { Outlet, NavLink, useNavigation, useLocation } from 'react-router';
+
+import styles from './RootLayout.module.scss';
 
 export const RootLayout = () => {
+  const navigation = useNavigation();
+  const location = useLocation();
+  const isNavigating = navigation.state === "loading";
+
+  const isCalendarActive = location.pathname.startsWith('/calendar') || location.pathname === '/';
+
   return (
-    <div>RootLayout</div>
-  )
+    <div className={styles.appContainer}>
+      <header>
+        <nav>
+          <NavLink 
+            to="/" 
+            className={() => isCalendarActive ? styles.active : ''}
+          >
+            Calendar
+          </NavLink>
+          
+          <NavLink 
+            to="/venues" 
+            className={({ isActive }: { isActive: boolean }) => 
+              isActive ? styles.active : ''
+            }
+          >
+            Venues
+          </NavLink>
+        </nav>
+      </header>
+
+      <main className={`${styles.main} ${isNavigating ? styles.fadeOut : ''}`}>
+        <Outlet /> 
+      </main>
+    </div>
+  );
 }
