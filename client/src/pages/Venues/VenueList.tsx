8437b69b6448434d488bae36f0672cfcc6143c16
Seth Zarkovich <sethzarkovich@gmail.com>
2026-05-31 20:40:46 -0400

    adds pagination component and Venues list view

--- client/src/pages/Venues/VenueList.tsx
+++ client/src/pages/Venues/VenueList.tsx
@@ -1,6 +1,42 @@
+import { useLoaderData } from "react-router";
+import { Pagination } from "../../components/Pagination/Pagination";
+
+import type { Venue } from "../../types/IVenue";
+
+import styles from './VenueList.module.scss'
+
+interface VenueLoaderData {
+  venues: Venue[];
+  currentPage: number;
+  totalPages: number;
+}
 
 export const VenueList = () => {
+  const data = useLoaderData() as VenueLoaderData
+
+  if (!data) {
+    return <div className={styles.error}>No Venues Found</div>;
+  }
+
+  const { venues, currentPage, totalPages } = data;
+
   return (
-    <div>VenueList</div>
-  )
-}
+    <div className={styles.container}>
+      <h1>Venues</h1>
+
+      {venues.length === 0 ? (
+        <p>No venues found.</p>
+      ) : (
+        <ul>
+          {venues.map((venue: Venue) => (
+            <li key={venue.venue_id}>
+              <strong>{venue.name}</strong> {venue.street} - {venue.city}, {venue.province}
+            </li>
+          ))}
+        </ul>
+      )}
+
+      <Pagination currentPage={currentPage} totalPages={totalPages} />
+    </div>
+  );
+};
---
+++ client/src/types/IVenue.ts
@@ -0,0 +1,8 @@
+export interface Venue {
+  venue_id: number;
+  name: string;
+  street: string;
+  city: string;
+  province: string;
+  postalCode: string;
+}
